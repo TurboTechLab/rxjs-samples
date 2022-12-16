0f7e2aa70f9aa5345feaf66f12e8d01d9d6531be
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-16 15:07:51 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import './style.css';
 
-import { of, map, Observable } from 'rxjs';
+import { merge, map, fromEvent, startWith } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
+
+const start = fromEvent(document.getElementById('start'),'click')
+              .pipe(map(v=>'start'));
+const pause = fromEvent(document.getElementById('start'),'click')
+              .pipe(map(v=>'pause'));
+const reset = fromEvent(document.getElementById('start'),'click')
+              .pipe(map(v=>'reset'));
+
+const stopwatch = merge(start, pause, reset)
+  .pipe(
+    startWith('reset'),
+    map((name) => `Hello, ${name}!`))
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
