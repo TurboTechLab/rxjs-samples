d5ca39f5ff81f6f0bec4bb1042e161b1ef30acac
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-16 15:20:56 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,19 +1,32 @@
 import './style.css';
+import { merge, map, switchMap } from 'rxjs';
+import { interval, EMPTY, fromEvent, startWith } from 'rxjs';
 
-import { merge, map, fromEvent, startWith } from 'rxjs';
-
-
-const start = fromEvent(document.getElementById('start'),'click')
-              .pipe(map(v=>'start'));
-const pause = fromEvent(document.getElementById('pause'),'click')
-              .pipe(map(v=>'pause'));
-const reset = fromEvent(document.getElementById('reset'),'click')
-              .pipe(map(v=>'reset'));
+const start = fromEvent(document.getElementById('start'), 'click').pipe(
+  map((v) => 'start')
+);
+const pause = fromEvent(document.getElementById('pause'), 'click').pipe(
+  map((v) => 'pause')
+);
+const reset = fromEvent(document.getElementById('reset'), 'click').pipe(
+  map((v) => 'reset')
+);
+const speed = 1000;
+let counterValue = 0;
 
 const stopwatch = merge(start, pause, reset)
   .pipe(
     startWith('reset'),
-    map((name) => `Hello, ${name}!`))
+    switchMap((status) => {
+      if (status == 'reset') {
+        return EMPTY.pipe(startWith(0));
+      }else  if (status == 'pause') {
+          return EMPTY;
+      } else {
+        return interval(speed);
+      }
+    })
+  )
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
