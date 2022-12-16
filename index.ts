fac18174654b5ff18614497ebc7a577d495cfcf1
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-16 21:29:37 +0530

    simplified version

--- index.ts
+++ index.ts
@@ -3,11 +3,16 @@ import { merge, switchMap, tap, of } from 'rxjs';
 import { interval, EMPTY, startWith } from 'rxjs';
 import { start, pause, reset } from './button-events';
 
-const speed = 1000;
+const tickDuration = 1000;
 let counterValue = 0;
 
-const updateStopwatch = () => {
-  document.getElementById('stopwatch').innerText = counterValue+'';
+const tickStopwatch = () => {
+  counterValue++;
+  document.getElementById('stopwatch').innerText = counterValue + '';
+};
+const resetStopwatch = () => {
+  counterValue = 0;
+  document.getElementById('stopwatch').innerText = counterValue + '';
 };
 
 const stopwatch = merge(start, pause, reset)
@@ -17,14 +22,14 @@ const stopwatch = merge(start, pause, reset)
     //Switch between multiple observables
     switchMap((status) => {
       if (status == 'reset') {
-        counterValue = 0;
-        return of(counterValue);
+        resetStopwatch();
+        return EMPTY;
       } else if (status == 'pause') {
         return EMPTY;
       } else {
-        return interval(speed).pipe(tap((v) => counterValue++));
+        return interval(tickDuration);
       }
     }),
-    tap((v) => updateStopwatch())
+    tap((v) => tickStopwatch())
   )
   .subscribe();
